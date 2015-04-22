917e40d7d1691dae621ec3320738afc163528b9d
QuentinC <webmaster@quentinc.net>
2015-04-22 18:25:12 +0200

    Renamed documentation => doc

--- d.bat
+++ d.bat
@@ -1,2 +1,2 @@
 @echo off
-for /r %%i in (documentation\*.md) do pandoc %%i -o documentation\%%~ni.html -s -S -t html5 -p --normalize --toc --self-contained -f markdown+lists_without_preceding_blankline+compact_definition_lists-blank_before_header
+for /r %%i in (doc\*.md) do pandoc %%i -o doc\%%~ni.html -s -S -t html5 -p --normalize --toc --self-contained -f markdown+lists_without_preceding_blankline+compact_definition_lists-blank_before_header
