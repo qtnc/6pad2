8ba26fce803355ce877b6faaa743da08a19b6c72
QuentinC <webmaster@quentinc.net>
2015-05-24 22:18:01 +0200

    Removed console for release version

--- main/r.bat
+++ main/r.bat
@@ -2,5 +2,5 @@
 del obj\*.o
 windres res.rc -o obj\rc.o
 for %%i in (*.cpp) do g++ -c -s -O3  -std=gnu++11 %%i -w -o obj\%%~ni.o -I../core -DRELEASE
-g++ -specs=msvcr100 -s -O3  -w -o "..\6pad++.exe" obj\*.o -luser32 -lkernel32 -lcomdlg32 -lcomctl32 -lgdi32 -lwinmm -lshlwapi -lboost-regex -L../link -lUniversalSpeech -lpython34 -lqc6pad10 -mthreads -m32 -DRELEASE
+g++ -specs=msvcr100 -s -O3  -w -o "..\6pad++.exe" obj\*.o -luser32 -lkernel32 -lcomdlg32 -lcomctl32 -lgdi32 -lwinmm -lshlwapi -lboost-regex -L../link -lUniversalSpeech -lpython34 -lqc6pad10 -mthreads -mwindows -m32 -DRELEASE
 del obj\*.o
